ba8931015c0897c84081867ac7ab1d12c08c67c7
azaltura <azaltura@gmail.com>
2019-02-25 21:49:41 +0200

    http_server

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 const express = require('express');
 const cors = require('cors');
 const PORT = 8888;
-const app = express();            
+const app = express();
+const  mysql = require('mysql');
+const fs = require('fs');
+const rl = require('readline');
+const dns = require('dns');
+
+
+
 app.use(cors());
-app.get('/student', function(req, res) {
-    const students = [
-        {
-            id: 1,
-            name:  'ariel'
-        },
-        {
-            id: 2,
-            name: 'haha'
-        }
-    ]
-    res.send(JSON.stringify(students));
+
+
+
+app.get('/student', function (req, res) {
+    
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'student'
+
+  });      
+    
+
+    connection.connect();
+    connection.query('SELECT * FROM `students`', function (err, results, fields) {
+        if (err) {throw err;}
+        console.log(results.RowDataPacket);
+        connection.end();
+        res.send(JSON.stringify(results));
+
+    });
+
+    
 })
 
-app.listen(PORT, function() {
+
+
+
+app.listen(PORT, function () {
     console.log('server started at port ' + PORT)
 });
+
